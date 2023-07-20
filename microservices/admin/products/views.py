faca724182e558c5ab99f5de08266ed1c45c8fe2
purveshmakode24 <makodepurvesh@gmail.com>
2023-07-20 06:07:55 +0530

    Add Transaction Processing to handle data inconsistency due to data-not-saving or publish failure

--- microservices/admin/products/views.py
+++ microservices/admin/products/views.py
@@ -7,42 +7,63 @@ from rest_framework.views import APIView
 from .serializers import ProductSerializer
 from rest_framework import status
 from .producer import publish
+from django.db import transaction
 import random
 
 # Create your views here.
 
 class ProductViewSet(viewsets.ViewSet):
+
     def list(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
+    """
+    Added Transaction to handle data inconsistency due to publish failure.
+    """
+    @transaction.atomic
     def create(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        print('test product created!')
-        publish('product_created', serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        try:
+            serializer = ProductSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            print('test product created!')
+            publish('product_created', serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception as e:
+            transaction.set_rollback(True)  # Rollback the transaction on exception
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def retrieve(self, reqest, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
+    @transaction.atomic
     def update(self, request, pk=None):
-        product = Product.objects.get(id=pk)
-        serializer = ProductSerializer(instance=product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        publish('product_updated', serializer.data)
-        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        try:
+            product = Product.objects.get(id=pk)
+            serializer = ProductSerializer(instance=product, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            publish('product_updated', serializer.data)
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            transaction.set_rollback(True)  # Rollback the transaction on exception
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    @transaction.atomic
     def destroy(self, request, pk=None):
-        product = Product.objects.get(id=pk)
-        product.delete()
-        publish('product_deleted', pk)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        try:
+            product = Product.objects.get(id=pk)
+            product.delete()
+            publish('product_deleted', pk)
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Exception as e:
+            transaction.set_rollback(True)  # Rollback the transaction on exception
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class UserAPIView(APIView):
     def get(self, _):
